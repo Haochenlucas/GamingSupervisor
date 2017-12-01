1631017c26e7ab13a9920e2b12ef42c3ce6c6b6f
theoculus8 <theoculus8@gmail.com>
2017-12-01 10:48:05 -0700

    Add parser

--- GamingSupervisor/GamingSupervisor/Program.cs
+++ GamingSupervisor/GamingSupervisor/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GamingSupervisor
 {
     class Program
     {
+        static private void ProcessLine(string line)
+        {
+
+        }
+
+        static private void ReadDotaLog(string fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (true)
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            ProcessLine(sr.ReadLine());
+                        }
+                        while (sr.EndOfStream)
+                        {
+                            Thread.Sleep(10);
+                        }
+                        ProcessLine(sr.ReadLine());
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            ReadDotaLog("");
         }
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,17 +16,26 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        public string difficulty;
-        
+        public string difficulty;        
 
-        public Boolean hero_selection = false;
-        public Boolean item_helper = false;
-        public Boolean laning = false;
-        public Boolean last_hit = false;
-        public Boolean jungling = false;
+        public Boolean hero_selection    = false;
+        public Boolean item_helper       = false;
+        public Boolean laning            = false;
+        public Boolean last_hit          = false;
+        public Boolean jungling          = false;
         public Boolean safe_farming_area = false;
 
-        public int state = 0;
+        public Boolean replay_selected = false;
+
+        public enum State
+        {
+            select_difficulty,
+            customize,
+            game_type,
+            start
+        };
+
+        public State state = State.select_difficulty;
 
         public string filename;
 
@@ -35,34 +47,34 @@ namespace WindowsFormsApp1
             this.MaximizeBox = false;
 
             checkbox_container.Left = (this.ClientSize.Width - checkbox_container.Width) / 2;
-            checkbox_container.Top = (this.ClientSize.Height - checkbox_container.Height) / 2;
+            checkbox_container.Top  = (this.ClientSize.Height - checkbox_container.Height) / 2;
             checkbox_container.Hide();
             
             title_label.Left = (this.ClientSize.Width - title_label.Width) / 2;
 
             novice_button.Left = (this.ClientSize.Width - novice_button.Width) / 2;
-            novice_button.Top = (this.ClientSize.Height - novice_button.Height) / 2 - 100;
+            novice_button.Top  = (this.ClientSize.Height - novice_button.Height) / 2 - 100;
 
             learning_button.Left = (this.ClientSize.Width - novice_button.Width) / 2;
-            learning_button.Top = (this.ClientSize.Height - novice_button.Height) / 2;
+            learning_button.Top  = (this.ClientSize.Height - novice_button.Height) / 2;
 
             almost_button.Left = (this.ClientSize.Width - novice_button.Width) / 2;
-            almost_button.Top = (this.ClientSize.Height - novice_button.Height) / 2 + 100;
+            almost_button.Top  = (this.ClientSize.Height - novice_button.Height) / 2 + 100;
 
             player_level_text.Left = title_label.Left;
-            player_level.Left = player_level_text.Right;
+            player_level.Left      = player_level_text.Right;
 
             replay_button.Left = (this.ClientSize.Width - replay_button.Width) / 2;
-            replay_button.Top = (this.ClientSize.Height + player_level.Bottom - replay_button.Width) / 2 - 50;
+            replay_button.Top  = (this.ClientSize.Height + player_level.Bottom - replay_button.Width) / 2 - 50;
 
             live_button.Left = (this.ClientSize.Width - live_button.Width) / 2;
-            live_button.Top = (this.ClientSize.Height + player_level.Bottom - live_button.Width) / 2 + 50;
+            live_button.Top  = (this.ClientSize.Height + player_level.Bottom - live_button.Width) / 2 + 50;
 
             go_button.Left = (this.ClientSize.Width - go_button.Width) / 2;
-            go_button.Top = (this.ClientSize.Height + player_level.Bottom - go_button.Width) / 2;
+            go_button.Top  = (this.ClientSize.Height + player_level.Bottom - go_button.Width) / 2;
 
             timer_text.Left = (this.ClientSize.Width - go_button.Width) / 2;
-            timer_text.Top = (this.ClientSize.Height + player_level.Bottom - go_button.Width) / 2;
+            timer_text.Top  = (this.ClientSize.Height + player_level.Bottom - go_button.Width) / 2;
         }
         
         private void novice_button_Click(object sender, EventArgs e)
@@ -80,7 +92,7 @@ namespace WindowsFormsApp1
             cb_confirm.Show();
             back_button.Show();
 
-            state++;
+            state = State.customize;
         }
 
         private void learning_button_Click(object sender, EventArgs e)
@@ -98,7 +110,7 @@ namespace WindowsFormsApp1
             cb_confirm.Show();
             back_button.Show();
 
-            state++;
+            state = State.customize;
         }
 
         private void almost_button_Click(object sender, EventArgs e)
@@ -116,7 +128,7 @@ namespace WindowsFormsApp1
             cb_confirm.Show();
             back_button.Show();
 
-            state++;
+            state = State.customize;
         }
 
         private void lh_checkbox_CheckedChanged(object sender, EventArgs e)
@@ -163,7 +175,7 @@ namespace WindowsFormsApp1
             }
             else
             {
-                state++;
+                state = State.game_type;
                 checkbox_container.Hide();
                 cb_confirm.Hide();
 
@@ -174,46 +186,60 @@ namespace WindowsFormsApp1
 
         private void back_button_Click(object sender, EventArgs e)
         {
-            if (state == 1)
+            switch (state)
             {
-                cb_confirm.Hide();
-                back_button.Hide();
-                checkbox_container.Hide();
-                player_level.Hide();
-                player_level_text.Hide();
+                case State.customize:
+                    cb_confirm.Hide();
+                    back_button.Hide();
+                    checkbox_container.Hide();
+                    player_level.Hide();
+                    player_level_text.Hide();
 
-                novice_button.Show();
-                learning_button.Show();
-                almost_button.Show();
+                    novice_button.Show();
+                    learning_button.Show();
+                    almost_button.Show();
+
+                    state = State.select_difficulty;
+                    break;
+                case State.game_type:
+                    cb_confirm.Show();
+                    checkbox_container.Show();
+
+                    replay_button.Hide();
+                    live_button.Hide();
+
+                    state = State.customize;
+                    break;
+                case State.start:
+                    go_button.Hide();
+
+                    replay_button.Show();
+                    live_button.Show();
+
+                    state = State.game_type;
+                    break;
             }
-            else if (state == 2)
-            {
-                cb_confirm.Show();
-                checkbox_container.Show();
-
-                replay_button.Hide();
-                live_button.Hide();
-            }
-            else if (state == 3)
-            {
-                go_button.Hide();
-
-                replay_button.Show();
-                live_button.Show();
-            }
-
-            state--;
         }
 
         private void replay_button_Click(object sender, EventArgs e)
         {
+            openFileDialog1.Filter = "dem files (*.dem)|*.dem";
+            openFileDialog1.Title = "Select a replay file";
             DialogResult result = openFileDialog1.ShowDialog();
-            state++;
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                return;
+            }
+
+            state = State.start;
 
             replay_button.Hide();
             live_button.Hide();
 
             go_button.Show();
+
+            replay_selected = true;
         }
 
         public int timeleft = 5;
@@ -224,12 +250,38 @@ namespace WindowsFormsApp1
             filename = ofd.FileName;
         }
 
+        private void parseReplayFile()
+        {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute        = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName               = "java";
+            p.StartInfo.Arguments =        
+                "-jar " 
+                + Path.Combine(Environment.CurrentDirectory, @"..\..\Parser\parser.jar ")
+                + filename.Replace(@"\", @"\\")
+                + " "
+                + Path.Combine(Environment.CurrentDirectory, @"..\..\Parser\"); // Data dump location
+            p.Start();
+
+            //String s = p.StandardOutput.ReadToEnd();
+            //Console.Write(s);
+            p.WaitForExit();
+            Console.WriteLine("Finished parsing!");
+        }
+
         private void go_button_Click(object sender, EventArgs e)
         {
             go_button.Hide();
             timer_text.Show();
             back_button.Hide();
+
+            Thread thread = new Thread(parseReplayFile);
+            thread.Start();
+
             timer1.Start();
+
+            //uthread.Join();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
