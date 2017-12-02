e0fb0f276401dc4f365f4891d8a670fee1346694
theoculus8 <theoculus8@gmail.com>
2017-12-02 09:01:35 -0700

    More rename changes I missed

--- GamingSupervisor/GamingSupervisor/Properties/Resources.Designer.cs
+++ GamingSupervisor/GamingSupervisor/Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace GamingSupervisor.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsApp1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GamingSupervisor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
