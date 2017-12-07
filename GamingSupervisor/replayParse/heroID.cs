a3e3eb3f1e477efab97d2d2861a28a8b58d4d958
yuchengy <yuchengy@LAB3B-1>
2017-12-07 15:16:36 -0700

    add replayParse into gaming supervisor

---
+++ GamingSupervisor/replayParse/heroID.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heroIDtable
+{
+    class heroID
+    {
+        static Dictionary<int, string> hero_IDDictionary = new Dictionary<int, string>();
+        static Dictionary<string, int> ID_heroDictionary = new Dictionary<string, int>();
+        public heroID()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"X:\data_info\dota_hero_info_1.txt");
+            string[] second_lines = lines;
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            int key = 0;
+            int length_name = 0;
+            foreach (string line in lines)
+            {
+                length_name = 0;
+                string second_string = string.Empty;
+                string[] words = line.Split('\t');
+                string[] name = words[0].Split(' ');
+                length_name = name.Length / 2;
+                string hero_name = "";
+                for (int i = 0; i < length_name; i++)
+                {
+                    if (i == 0)
+                        hero_name = name[0];
+                    else
+                        hero_name = hero_name + " " + name[i];
+                }
+                second_string = (key + 1) + "     " + hero_name;
+                second_lines[key] = second_string;
+                key++;
+                hero_IDDictionary.Add(key, words[0]);
+                ID_heroDictionary.Add(words[0], key);
+            }
+            string path = @"X:\data_info\heroIDtable.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    foreach (string line in second_lines)
+                    {
+                        sw.WriteLine(line);
+                    }
+
+                }
+            }
+        }
+
+        public heroID(string filePath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            string[] second_lines = lines;
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            int key = 0;
+            int length_name = 0;
+            foreach (string line in lines)
+            {
+                length_name = 0;
+                string second_string = string.Empty;
+                string[] words = line.Split('\t');
+                string[] name = words[0].Split(' ');
+                length_name = name.Length / 2;
+                string hero_name = "";
+                for (int i = 0; i < length_name; i++)
+                {
+                    if (i == 0)
+                        hero_name = name[0];
+                    else
+                        hero_name = hero_name + " " + name[i];
+                }
+                second_string = (key + 1) + "     " + hero_name;
+                second_lines[key] = second_string;
+                key++;
+                hero_IDDictionary.Add(key, words[0]);
+                ID_heroDictionary.Add(words[0], key);
+            }
+            string path = @"X:\data_info\heroIDtable.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    foreach (string line in second_lines)
+                    {
+                        sw.WriteLine(line);
+                    }
+
+                }
+            }
+        }
+
+        public Dictionary<int, string> getHeroID()
+        {
+            return hero_IDDictionary;
+        }
+        public Dictionary<string, int> getIDHero()
+        {
+            return ID_heroDictionary;
+        }
+
+
+    }
+}
---
+++ GamingSupervisor/replayParse/replayParse.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace replayParse
+{
+    
+    public class replay_version01
+    {
+        public static int[,,] replayinfo = new int[200000, 10, 4];
+        private int[,] prev_stat = new int[10, 4]; // first index is heroID for this match, the second index is some info: 0: health, 1: cell_x, 2: cell_y, 3 cell_z;
+        public static Dictionary<string, int> heros = new Dictionary<string, int>();
+        public int[] sideOfHero = new int[10];  // the index is the heroID: the sort of ID show the sequence of picking , the number in string shows the side of heros. 0: for one side, 1 : for another side.
+        public int offsetTic = 0;
+        public replay_version01()
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(@"X:\data_info\replay.txt");
+            int tic = 0;
+            int value = 0;
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(' ');
+                int time = Int32.Parse(words[0]);
+                if (tic == 0)
+                {
+                    offsetTic = time;
+                    tic = time;
+                }
+                else
+                {
+                    while (time > tic)
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                if (prev_stat[i, j] != 0)
+                                {
+                                    replayinfo[tic - offsetTic, i, j] = prev_stat[i, j];
+                                }
+                            }
+                        }
+                        tic++;
+                    }
+                    tic = time;
+                }
+                int mode = 0;
+                int heroID = 0;
+                if (words[1].Contains('P'))
+                {
+                    mode = 1;
+                }
+                string[] substrings = Regex.Split(words[2], "Hero_");
+                if (!heros.Keys.Contains(substrings[1]))
+                {
+                    heros.Add(substrings[1], value);
+                    heroID = heros[substrings[1]];
+                    if (Int32.Parse(words[3]) > 100)
+                    {
+                        sideOfHero[heroID] = 1;
+                    }
+                    value++;
+                }
+                else
+                {
+                    heroID = heros[substrings[1]];
+                }
+                if (mode == 1)
+                {
+                    if (words.Length < 6)
+                    {
+                        throw new System.ArgumentOutOfRangeException("lost position information");
+                    }
+
+                    replayinfo[time - offsetTic, heroID, 1] = Int32.Parse(words[3]);
+                    replayinfo[time - offsetTic, heroID, 2] = Int32.Parse(words[4]);
+                    replayinfo[time - offsetTic, heroID, 3] = Int32.Parse(words[5]);
+                    prev_stat[heroID, 1] = Int32.Parse(words[3]);
+                    prev_stat[heroID, 2] = Int32.Parse(words[4]);
+                    prev_stat[heroID, 3] = Int32.Parse(words[5]);
+                }
+                else
+                {
+                    replayinfo[time - offsetTic, heroID, 0] = Int32.Parse(words[3]);
+                    prev_stat[heroID, 0] = Int32.Parse(words[3]);
+                }
+            }
+        }
+
+        public replay_version01(string fileAddress)
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(fileAddress);
+            int tic = 0;
+            int value = 0;
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(' ');
+                int time = Int32.Parse(words[0]);
+                if (tic == 0)
+                {
+                    offsetTic = time;
+                    tic = time;
+                }
+                else
+                {
+                    while (time > tic)
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                if (prev_stat[i, j] != 0)
+                                {
+                                    replayinfo[tic - offsetTic, i, j] = prev_stat[i, j];
+                                }
+                            }
+                        }
+                        tic++;
+                    }
+                    tic = time;
+                }
+                int mode = 0;
+                int heroID = 0;
+                if (words[1].Contains('P'))
+                {
+                    mode = 1;
+                }
+                string[] substrings = Regex.Split(words[2], "Hero_");
+                if (!heros.Keys.Contains(substrings[1]))
+                {
+                    heros.Add(substrings[1], value);
+                    heroID = heros[substrings[1]];
+                    if (Int32.Parse(words[3]) > 100)
+                    {
+                        sideOfHero[heroID] = 1;
+                    }
+                    value++;
+                }
+                else
+                {
+                    heroID = heros[substrings[1]];
+                }
+                if (mode == 1)
+                {
+                    replayinfo[time - offsetTic, heroID, 1] = Int32.Parse(words[3]);
+                    replayinfo[time - offsetTic, heroID, 2] = Int32.Parse(words[4]);
+                    replayinfo[time - offsetTic, heroID, 3] = Int32.Parse(words[5]);
+                    prev_stat[heroID, 1] = Int32.Parse(words[3]);
+                    prev_stat[heroID, 2] = Int32.Parse(words[4]);
+                    prev_stat[heroID, 3] = Int32.Parse(words[5]);
+                }
+                else
+                {
+                    replayinfo[time - offsetTic, heroID, 0] = Int32.Parse(words[3]);
+                    prev_stat[heroID, 0] = Int32.Parse(words[3]);
+                }
+            }
+        }
+
+        public Dictionary<string, int> getHeros()
+        {
+            return heros;
+        }
+
+        public int[,,] getReplayInfo()
+        {
+            return replayinfo;
+        }
+        public int getOffSet()
+        {
+            return offsetTic;
+        }
+        public int[] getHeroSide()
+        {
+            return sideOfHero;
+        }
+    }
+}
