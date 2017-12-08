ea244c316ef7a95c8775bc338e52c64fab5c1fcf
haochenz <haochenz@LAB3B-13>
2017-12-07 18:10:06 -0700

    redirect replayparse

--- GamingSupervisor/GamingSupervisor/Form1.cs
+++ GamingSupervisor/GamingSupervisor/Form1.cs
@@ -41,6 +41,7 @@ namespace GamingSupervisor
         private OverlayWindow window;
         private Direct2DRenderer d2d;
         private IntPtr dota_HWND;
+        private IntPtr VS_HWND;
 
         public enum State
         {
@@ -370,7 +371,7 @@ namespace GamingSupervisor
             parsing_label.Hide();
             back_button.Show();
             //Path.Combine(Environment.CurrentDirectory, @"..\..\Parser\")
-            string[] info = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"..\..\..\Parser\info.txt"));
+            string[] info = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"..\..\Parser\info.txt"));
             foreach (string test in info)
             {
                 if (test.Contains("hero_name"))
@@ -454,7 +455,8 @@ namespace GamingSupervisor
             if (Process.GetProcessesByName("dota2").Length > 0)
             {
                 dota_HWND = Process.GetProcessesByName("dota2")[0].MainWindowHandle;
-                overlayManager = new OverlayManager(dota_HWND, out window, out d2d);
+                VS_HWND = Process.GetProcessesByName("devenv")[0].MainWindowHandle;
+                overlayManager = new OverlayManager(VS_HWND, out window, out d2d);
             }
 
             StartAnalyzing();
@@ -466,7 +468,8 @@ namespace GamingSupervisor
             int health = 0;
             if (health < 200)
             {
-                d2d.retreat(dota_HWND, window, "Health is low, retreat");
+                string message = "Low Health";
+                d2d.retreat(VS_HWND, window, message);
             }
             else
             {
