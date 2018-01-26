2550685c76cb261f83c92b34f50a60141530e77e
theoculus8 <theoculus8@gmail.com>
2018-01-26 14:02:37 -0700

    Get game state from parser for better synchronizing

---
+++ Dota2Parser/src/main/java/Dota2Parser/Dota2Parser/GameState.java
@@ -0,0 +1,19 @@
+package Dota2Parser.Dota2Parser;
+
+import skadistats.clarity.model.Entity;
+import skadistats.clarity.model.FieldPath;
+
+public class GameState
+{
+	public FieldPath state;
+	
+	public GameState(Entity e)
+	{
+		state = e.getDtClass().getFieldPathForName("m_pGameRules.m_nGameState");
+	}
+	
+	public boolean isState(FieldPath path)
+	{
+		return path.equals(state);
+	}
+}
