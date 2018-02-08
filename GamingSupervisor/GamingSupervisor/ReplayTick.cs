541e77fb0a757879b9e8c6cc4a8f9230aa74e836
theoculus8 <theoculus8@gmail.com>
2018-02-08 15:30:00 -0700

    Fix synchronization issues (maybe)

---
+++ GamingSupervisor/GamingSupervisor/ReplayTick.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingSupervisor
+{
+    class ReplayTick
+    {
+        private Dictionary<int, int> gameTimeToTick;
+
+        public ReplayTick(string dataFolderLocation)
+        {
+            gameTimeToTick = new Dictionary<int, int>();
+            
+            foreach (string line in File.ReadLines(dataFolderLocation + "time.txt").Reverse())
+            {
+                string[] entries = line.Split(' ');
+                gameTimeToTick[(int)Convert.ToDouble(entries[2])] = Convert.ToInt32(entries[0]);
+            }
+        }
+
+        public int this[int i]
+        {
+            get => gameTimeToTick[i];
+        }
+    }
+}
