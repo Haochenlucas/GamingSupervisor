082e47bd8a5dd424190f7a7b616bfd682a008857
yuchengy <yuchengy@LAB3B-1>
2018-02-13 15:53:07 -0700

    herotype_version_0

---
+++ GamingSupervisor/replayParse/heroGenerateTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace replayParse
+{
+    public class heroGenerateTypes
+    {
+        private static int[,] hero_generate_type_table = new int[116, 12];
+
+        /*
+         * 
+         */
+        public heroGenerateTypes()
+        {
+
+        }
+    }
+}
