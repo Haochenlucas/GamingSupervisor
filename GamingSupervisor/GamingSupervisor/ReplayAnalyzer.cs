9ceb4eb41193428ff43be2a9db317f2700240545
Haochen Zhang <haochenlucas@gmail.com>
2018-03-05 14:32:34 -0700

    - item selection message box - hero information message box - instructions for selecting replay and camera mode

--- GamingSupervisor/GamingSupervisor/ReplayAnalyzer.cs
+++ GamingSupervisor/GamingSupervisor/ReplayAnalyzer.cs
@@ -218,7 +218,7 @@ namespace GamingSupervisor
             for (int j = 1; j < 6; j++)
             {
                 heroesimg[j - 1] = suggestiontable[index, j].ToString();
-                heroes[j - 1] = ID_table[suggestiontable[index, j]];
+                heroes[j - 1] = ID_table[suggestiontable[index, j]] + announcer.GetCurrentGameTime().ToString();
             }
             
             overlay.AddHeroesSuggestionMessage(heroes, heroesimg);
@@ -259,7 +259,7 @@ namespace GamingSupervisor
             }
             else
             {
-                overlay.ClearMessage(6);
+                overlay.ClearMessage(7);
             }
             
                 overlay.ToggleGraphForHeroHP();
