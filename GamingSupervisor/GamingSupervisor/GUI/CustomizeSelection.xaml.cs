a313a9af6ba235e15daa2182a894c8fe5f2548ec
theoculus8 <theoculus8@gmail.com>
2018-03-05 11:33:38 -0700

    Grammar fix

--- GamingSupervisor/GamingSupervisor/GUI/CustomizeSelection.xaml.cs
+++ GamingSupervisor/GamingSupervisor/GUI/CustomizeSelection.xaml.cs
@@ -13,14 +13,14 @@ namespace GamingSupervisor
         {
             InitializeComponent();
 
-            MainWindow.Description.Text = "Here you can customize what the Gaming Supervisor will help you do. Setting have already been selected for you based on what difficulty you selected.";
+            MainWindow.Description.Text = "Here you can customize what the Gaming Supervisor will help you with. Setting have already been selected for you based on what difficulty you selected.";
             MainWindow.Description.Text += "\n\n";
             MainWindow.Description.Text += "'Last hitting' is where you inflict the last hit on an enemy unit and kill them, which gives you in-game bonuses.";
             MainWindow.Description.Text += "\n\n";
             MainWindow.Description.Text += "'Hero selection' takes place at the beginning of the game, and involves selecting which hero you will play with.";
             MainWindow.Description.Text += " The hero you should pick is affected by the heros other players selected, heros the other team won't let you have (called 'banned'), and you skill level.";
             MainWindow.Description.Text += "\n\n";
-            MainWindow.Description.Text += "The item helper with help you with deciding when to use the items in your inventory.";
+            MainWindow.Description.Text += "The item helper will help you with deciding when to use the items in your inventory.";
             MainWindow.Description.Text += "\n\n";
             MainWindow.Description.Text += "In DotA 2, there are three 'lanes': the top lane, the middle lane, and the bottom lane. 'Laning' is when you attack and defend in the three lanes.";
             MainWindow.Description.Text += "\n\n";
