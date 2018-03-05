416c0a485c5d28da4442949760f238710ae0b257
Haochen Zhang <haochenlucas@gmail.com>
2018-03-05 15:07:47 -0700

    changes on types

--- GamingSupervisor/replayParse/counter_pick_logic.cs
+++ GamingSupervisor/replayParse/counter_pick_logic.cs
@@ -12,10 +12,11 @@ namespace replayParse
         // the hero_ID_Client_Team is all pick and ban hero, 
         // the second dimension first column is about hero_id,the second_column is about hero client id, the third_column is about team side, the fourth_column is about tic.
         // team side(0: (ban from team 1), 1: (ban from team 2) , 2: (pick from team 1), 3: (pick from team 2)).
-        public static int[,] hero_ID_Client_Team = new int[30,4];
+        public static int[,] hero_ID_Client_Team = new int[30, 4];
         // the table_suggestion is for each ban or pick tic, what heros we suggest to pick.
         // the second dimension first column is tic, 2 to 6 are the fice suggestion heros.
         public static int[,] table_suggestion = new int[25, 6];
+        public static int[,] HT_table;
 
         public counter_pick_logic(string dataFolderLocation)
         {
@@ -24,6 +25,8 @@ namespace replayParse
             counterpick_info cp_info = new counterpick_info();
             matrix_info = cp_info.getCounterTable();
             this.readTeam();
+            heroGenerateTypes hGT = new heroGenerateTypes();
+            HT_table = hGT.getTypeTable();
         }
 
 
@@ -61,9 +64,9 @@ namespace replayParse
             int ban = 0;
             foreach (string line in lines)
             {
-                if(mode == 1)
+                if (mode == 1)
                 {
-                    if (ban ==1)
+                    if (ban == 1)
                     {
                         if (line.Contains("team"))
                         {
@@ -72,13 +75,13 @@ namespace replayParse
                             int team_number = -1;
                             if (int.TryParse(words[words.Length - 1], out team_number))
                             {
-                                if(team_number == 2)
+                                if (team_number == 2)
                                 {
                                     hero_ID_Client_Team[count, 2] = 0;
                                 }
                                 else
                                 {
-                                    hero_ID_Client_Team[count, 2] = 1;                  
+                                    hero_ID_Client_Team[count, 2] = 1;
                                 }
                             }
 
@@ -110,12 +113,12 @@ namespace replayParse
                 {
                     mode = 1;
                     string[] words = line.Split(' ');
-                    if (words[words.Length-1].Contains("false"))
+                    if (words[words.Length - 1].Contains("false"))
                     {
                         ban = 1;
                     }
                 }
-                else if( mode == 2)
+                else if (mode == 2)
                 {
                     if (line.Contains("hero_id:"))
                     {
@@ -182,9 +185,9 @@ namespace replayParse
                 int pick_count = 0;
                 int[] ban_list = new int[17];
                 int ban_count = 0;
-                for (int i = 0; i < hero_ID_Client_Team.Length/4; i++)
+                for (int i = 0; i < hero_ID_Client_Team.Length / 4; i++)
                 {
-                    if (hero_ID_Client_Team[i,1] != 0)
+                    if (hero_ID_Client_Team[i, 1] != 0)
                     {
                         int tic = hero_ID_Client_Team[i, 3];
                         table_suggestion[table_count, 0] = tic;
@@ -199,7 +202,7 @@ namespace replayParse
                             pick_count++;
                         }
                         int[] suggest_cur = logic_counter(pick_list, ban_list);
-                        for(int j = 1; j < 6; j++)
+                        for (int j = 1; j < 6; j++)
                         {
                             table_suggestion[table_count, j] = suggest_cur[j - 1];
                         }
@@ -279,13 +282,13 @@ namespace replayParse
             double[] counterFact = new double[5];
             if (hero_sequence.Sum() == 0)
             {
-                foreach(KeyValuePair<double,int> pair in porper_pick)
+                foreach (KeyValuePair<double, int> pair in porper_pick)
                 {
                     if (!ban_list.Contains(pair.Value))
                     {
-                        five_picks.Add(pair.Key,pair.Value);
+                        five_picks.Add(pair.Key, pair.Value);
                     }
-                    if(five_picks.Count == 5)
+                    if (five_picks.Count == 5)
                     {
                         break;
                     }
@@ -332,7 +335,7 @@ namespace replayParse
                     count++;
                 }
             }
-            
+
             var list = five_picks.Keys.ToList();
             list.Sort();
 
@@ -355,9 +358,9 @@ namespace replayParse
          */
         public int[,] checkMark()
         {
-            int[,] checkTable = new int[25,3];
+            int[,] checkTable = new int[25, 3];
             int index = 0;
-            for(int i = 0; i < 25; i++)
+            for (int i = 0; i < 25; i++)
             {
                 int banorpick = 1;
                 int tic_1 = table_suggestion[i, 0];
@@ -366,28 +369,225 @@ namespace replayParse
                 int shootIndex = 0;
                 for (int j = 1; j < 6; j++)
                 {
-                    if(table_suggestion[i,j] == hero_ID_Client_Team[i + 1, 0])
+                    if (table_suggestion[i, j] == hero_ID_Client_Team[i + 1, 0])
                     {
-                        if(hero_ID_Client_Team[i + 1, 2]<= 1)
+                        if (hero_ID_Client_Team[i + 1, 2] <= 1)
                         {
                             banorpick = -1;
                         }
-                        shootIndex = (j-1)* banorpick;
+                        shootIndex = (j - 1) * banorpick;
                         checkTable[index, 0] = tic_2;
                         if ((tic_2 + (int)(tic_3 - tic_2) / 2) > 60)
                         {
-                            checkTable[index, 1] = tic_2+60;
+                            checkTable[index, 1] = tic_2 + 60;
                         }
                         else
                         {
                             checkTable[index, 1] = tic_2 + (int)(tic_3 - tic_2) / 2;
                         }
-                        checkTable[index, 2] = shootIndex; 
+                        checkTable[index, 2] = shootIndex;
                         index++;
                     }
                 }
             }
             return checkTable;
         }
+
+
+
+
+
+
+        /*
+         * version_1.1.0 logic_counter 
+         * Input: the hero_sequence which is heros picked by enemy team
+         *        the ban_list contains the heros picked by own team and all baned heros by both team.
+         * output: the best five hero we suggest to pick
+         * improve place: consider the role of the heros in the own team, consider the difficulty of the heros.
+         */
+        public static int[] logic_counter_1(int[] hero_sequence_enemy, int[] hero_sequence_teammate, int[] ban_list)
+        {
+            Dictionary<double, int> five_picks = new Dictionary<double, int>();
+            Dictionary<double, int> porper_pick = new Dictionary<double, int>();
+            // At least a team needs at least two Disabler[1], 
+            // better have one middle laner[10] and one offlaner[9],
+            // at least two support[4] and at least two carry[0], 
+            // no more than 4 support or no more 4 carry.
+            // 0:[Carry], 1: [Disabler] 2: [Initiator], 3:[Jungler], 4:[Support], 5:[Durable] 
+            // 6:[Nuker], 7: [Pusher], 8: [Escape], 9: [Offlane] 10: [Midlane]
+            int[] roleList = new int[5];
+
+            porper_pick.Add(1, 91);
+            porper_pick.Add(1.4, 46);
+            porper_pick.Add(1.6, 87);
+            porper_pick.Add(1.8, 105);
+            porper_pick.Add(2, 13);
+            porper_pick.Add(1.7, 15);
+            porper_pick.Add(1.2, 20);
+            int[] five_hero_array = new int[5];
+            int length = hero_sequence_enemy.Length;
+            int count = 1;
+            int flag = 0;
+            double lowest_fact = 100;
+            double[] counterFact = new double[5];
+            if (hero_sequence_enemy.Sum() == 0)
+            {
+                foreach (KeyValuePair<double, int> pair in porper_pick)
+                {
+                    if (!ban_list.Contains(pair.Value))
+                    {
+                        five_picks.Add(pair.Key, pair.Value);
+                    }
+                    if (five_picks.Count == 5)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                while (count <= 115)
+                {
+                    if (hero_sequence_enemy.Contains(count) || ban_list.Contains(count) || hero_sequence_teammate.Contains(count))
+                    {
+                        count++;
+                        continue;
+                    }
+                    double sum1 = 0;
+                    for (int i = 0; i < length; i++)
+                    {
+                        sum1 = sum1 + matrix_info[hero_sequence_enemy[i], count];
+                    }
+                    if (flag < 5)
+                    {
+                        if (five_picks.ContainsKey(sum1))
+                        {
+                            sum1 = sum1 + 0.001;
+                        }
+                        five_picks.Add(sum1, count);
+                        if (sum1 < lowest_fact)
+                            lowest_fact = sum1;
+                        flag++;
+                    }
+                    else if (flag >= 5)
+                    {
+                        if (sum1 > lowest_fact)
+                        {
+                            five_picks.Remove(lowest_fact);
+                            if (five_picks.ContainsKey(sum1))
+                            {
+                                sum1 = sum1 + 0.001;
+                            }
+                            five_picks.Add(sum1, count);
+                            lowest_fact = five_picks.Min(i => i.Key);
+                        }
+                    }
+                    count++;
+                }
+            }
+
+            var list = five_picks.Keys.ToList();
+            list.Sort();
+
+            // Loop through keys.
+            int index = 4;
+            foreach (var key in list)
+            {
+                five_hero_array[index] = five_picks[key];
+                index--;
+            }
+
+            return five_hero_array;
+        }
+
+
+        /*
+         *  A factor will used on pick logic. the factor is based on role for a whole team. 
+         *  The rule of this factor is 
+         *  // At least a team needs at least two Disabler[1], 
+            // better have one middle laner[10] and one offlaner[9],
+            // at least two support[4] and at least two carry[0], 
+            // no more than 4 support or no more 4 carry.
+            // no more than 3 midlane.
+            // 0:[Carry], 1: [Disabler] 2:[Support], 3: [Offlane] 4: [Midlane]
+            // 0:[Carry], 1: [Disabler] 2: [Initiator], 3:[Jungler], 4:[Support], 5:[Durable] 
+            // 6:[Nuker], 7: [Pusher], 8: [Escape], 9: [Offlane] 10: [Midlane]
+         */
+        public int role_factor(int[] roleList, int hero_id)
+        {
+            int roleFactor = 0;
+
+            int badAction = -10;
+            int earnAction = 1;
+            int greatAction = 5;
+
+            // no more than 4 support or no more 4 carry.
+            // no more than 3 midlane.
+            if (roleList[2] >= 4 && HT_table[hero_id, 4] == 1)
+            {
+                roleFactor = roleFactor + badAction;
+            }
+            if (roleList[0] >= 4 && HT_table[hero_id, 0] == 1)
+            {
+                roleFactor = roleFactor + badAction;
+            }
+            if (roleList[4] >= 3 && HT_table[hero_id, 10] == 1)
+            {
+                roleFactor = roleFactor + badAction;
+            }
+
+            // at least two support[4] and at least two carry[0], 
+            if (HT_table[hero_id, 4] == 1 && roleList[2] < 2)
+            {
+                roleFactor = roleFactor + greatAction;
+            }
+            if (HT_table[hero_id, 0] == 1 && roleList[0] < 2)
+            {
+                roleFactor = roleFactor + greatAction;
+            }
+
+            // better have one middle laner[10] and one offlaner[9],
+            if (HT_table[hero_id, 9] == 1 && roleList[3] == 0)
+            {
+                roleFactor = roleFactor + greatAction;
+            }
+            if (HT_table[hero_id, 10] == 1 && roleList[4] == 0)
+            {
+                roleFactor = roleFactor + greatAction;
+            }
+
+            // At least a team needs at least two Disabler[1], 
+            if (HT_table[hero_id, 1] == 1 && roleList[1] < 2)
+            {
+                roleFactor = roleFactor + greatAction;
+            }
+
+            if (HT_table[hero_id, 1] == 1)
+            {
+                roleFactor = roleFactor + earnAction;
+            }
+            if (HT_table[hero_id, 5] == 1)
+            {
+                roleFactor = roleFactor + earnAction;
+            }
+            if (HT_table[hero_id, 6] == 1)
+            {
+                roleFactor = roleFactor + earnAction;
+            }
+            if (HT_table[hero_id, 7] == 1)
+            {
+                roleFactor = roleFactor + earnAction;
+            }
+            if (HT_table[hero_id, 8] == 1)
+            {
+                roleFactor = roleFactor + earnAction;
+            }
+
+
+
+            return roleFactor;
+        }
+
+
     }
 }
