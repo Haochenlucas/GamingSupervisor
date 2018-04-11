60c166463f6404e9013dbb2e0375c776ad20df67
Haochen Zhang <haochenlucas@gmail.com>
2018-04-11 16:10:30 -0600

    create new class for jungle helper

---
+++ GamingSupervisor/replayParse/JungleCampData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace replayParse
+{
+    // This class include all jungle camps position, stacking timer and images representing the direction of wave pulling for each camp
+    // Further more, It can include all neutral monsters' data
+    class JungleCampData
+    {
+        // Hard coded position x,y
+        private Tuple<double,double>[] jungleCampPos = new Tuple<double, double>[18];
+        // Timer to pull the wave
+        private int[] campMinMark = new int[18];
+
+        public JungleCampData()
+        {
+            for(int i = 0; i < 18; i++)
+            {
+
+            }
+        }
+
+        public Tuple<double, double> GetCampPos(int index)
+        {
+            return jungleCampPos[index];
+        }
+
+        public int GetCampMinMark(int index)
+        {
+            return campMinMark[index];
+        }
+
+        public string GetDirectionImg(int index)
+        {
+            return "";
+        }
+    }
+}
