877f2430434edc9f739b016dfcc2cfe1d4a1be38
Haochen Zhang <haochenlucas@gmail.com>
2018-04-12 19:38:49 -0600

    creating jungle class

--- GamingSupervisor/replayParse/JungleCampData.cs
+++ GamingSupervisor/replayParse/JungleCampData.cs
@@ -14,12 +14,75 @@ namespace replayParse
         private Tuple<double,double>[] jungleCampPos = new Tuple<double, double>[18];
         // Timer to pull the wave
         private int[] campMinMark = new int[18];
+        // Bitmap image for DirectionImg
+        // DrawBitmap(bmp, 0.2f, messages[i].img_x, messages[i].img_y - modifier, messages[i].img_width, messages[i].img_height);
+        private Tuple<string, float, float, float, float>[] DirectionImg = new Tuple<string, float, float, float, float>[18];
 
         public JungleCampData()
         {
+            // Order the camps by difficulties
             for(int i = 0; i < 18; i++)
             {
-
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine("Case 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 4:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 5:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 6:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 7:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 8:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 9:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 10:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 11:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 12:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 13:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 14:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 15:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 16:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 17:
+                        Console.WriteLine("Case 2");
+                        break;
+                    case 18:
+                        Console.WriteLine("Case 2");
+                        break;
+                    default:
+                        Console.WriteLine("Default case");
+                        break;
+                }
             }
         }
 
@@ -33,9 +96,9 @@ namespace replayParse
             return campMinMark[index];
         }
 
-        public string GetDirectionImg(int index)
+        public Tuple<string, float, float, float, float> GetDirectionImg(int index)
         {
-            return "";
+            return DirectionImg[index];
         }
     }
 }
